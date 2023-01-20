395d97f22f92b5fd4b065aedc1ee4e6cad199f7b
Oleh-UmerT <olegus2727@gmail.com>
2023-01-20 11:02:51 +0200

    added circle butons to mome to selected elem

--- src/App.js
+++ src/App.js
@@ -3,18 +3,39 @@ import React from "react";
 
 function App() {
   const [moveOn, setMoveOn] = React.useState(0);
+  const [activeButton, setActiveButton] = React.useState("0");
 
   const handleClick = (event) => {
     if (event.target.id === "after") {
       //if we are on 3rd slide - move to 1st, else just move forward on 300px
-      moveOn > -600
-        ? setMoveOn((prev) => prev - 300)
-        : setMoveOn((prev) => prev + 600);
+      if (moveOn > -600) {
+        setMoveOn((prev) => prev - 300);
+        //+() - convert to Number
+        setActiveButton((prev) => String(+prev + 1));
+      } else {
+        setMoveOn((prev) => prev + 600);
+        setActiveButton("0");
+      }
     } else {
       //if we are om 1st slide - move to 3rd, else just move backward on 300px
-      moveOn < 0 ? setMoveOn((prev) => prev + 300) : setMoveOn((prev) => prev - 600);
+      if (moveOn < 0) {
+        setMoveOn((prev) => prev + 300);
+        //+() - convert to Number
+        setActiveButton((prev) => String(+prev - 1));
+      } else {
+        setMoveOn((prev) => prev - 600);
+        setActiveButton("2");
+      }
     }
   };
+
+  const handleClickActive = (event) => {
+    return setActiveButton(event.target.id);
+  };
+  React.useEffect(() => {
+    //+() - convert to Number
+    setMoveOn(-300 * +activeButton);
+  }, [activeButton]);
 
   return (
     <div className="App">
@@ -23,27 +44,22 @@ function App() {
       </button>
       <div className="row">
         <div
+          id="first"
           style={{
-            backgroundColor: "red",
-            width: "200px",
-            marginLeft: "200px",
             //"move" line:
             transform: `translateX(${moveOn}px)`,
           }}
         ></div>
         <div
+          id="second"
           style={{
-            backgroundColor: "red",
-            width: "200px",
-            height: "100px",
             //"move" line:
             transform: `translateX(${moveOn}px)`,
           }}
         ></div>
         <div
+          id="third"
           style={{
-            backgroundColor: "red",
-            width: "200px",
             //"move" line:
             transform: `translateX(${moveOn}px)`,
           }}
@@ -52,6 +68,26 @@ function App() {
       <button onClick={handleClick} id="after">
         After
       </button>
+      <div className="buttons">
+        <button
+          id="0"
+          className="circles"
+          style={activeButton === "0" ? { backgroundColor: "green" } : null}
+          onClick={handleClickActive}
+        ></button>
+        <button
+          id="1"
+          className="circles"
+          style={activeButton === "1" ? { backgroundColor: "green" } : null}
+          onClick={handleClickActive}
+        ></button>
+        <button
+          id="2"
+          className="circles"
+          style={activeButton === "2" ? { backgroundColor: "green" } : null}
+          onClick={handleClickActive}
+        ></button>
+      </div>
     </div>
   );
 }
