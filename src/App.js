320ffcd8c6201ef2cc9a3b4d73d9a59bcd3e6558
Oleh-UmerT <olegus2727@gmail.com>
2023-01-20 10:06:45 +0200

    simple slider app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [moveOn, setMoveOn] = React.useState(0);
+
+  const handleClick = (event) => {
+    if (event.target.id === "after") {
+      //if we are on 3rd slide - move to 1st, else just move forward on 300px
+      moveOn > -600
+        ? setMoveOn((prev) => prev - 300)
+        : setMoveOn((prev) => prev + 600);
+    } else {
+      //if we are om 1st slide - move to 3rd, else just move backward on 300px
+      moveOn < 0 ? setMoveOn((prev) => prev + 300) : setMoveOn((prev) => prev - 600);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick} id="before">
+        Before
+      </button>
+      <div className="row">
+        <div
+          style={{
+            backgroundColor: "red",
+            width: "200px",
+            marginLeft: "200px",
+            //"move" line:
+            transform: `translateX(${moveOn}px)`,
+          }}
+        ></div>
+        <div
+          style={{
+            backgroundColor: "red",
+            width: "200px",
+            height: "100px",
+            //"move" line:
+            transform: `translateX(${moveOn}px)`,
+          }}
+        ></div>
+        <div
+          style={{
+            backgroundColor: "red",
+            width: "200px",
+            //"move" line:
+            transform: `translateX(${moveOn}px)`,
+          }}
+        ></div>
+      </div>
+      <button onClick={handleClick} id="after">
+        After
+      </button>
     </div>
   );
 }
